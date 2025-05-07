7f15cea248f69bf730d30362669e2521b0ac9c2a
Lisa <amorimbernardo2005@gmail.com>
2025-05-06 21:13:04 -0300

    first commit - first lexer implementation and AST beginning

---
+++ build/CMakeFiles/MedusaCompiler.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bernardo/projects/cpp_projects/MedusaCompiler/AST.cpp" "CMakeFiles/MedusaCompiler.dir/AST.cpp.o" "gcc" "CMakeFiles/MedusaCompiler.dir/AST.cpp.o.d"
+  "/home/bernardo/projects/cpp_projects/MedusaCompiler/App.cpp" "CMakeFiles/MedusaCompiler.dir/App.cpp.o" "gcc" "CMakeFiles/MedusaCompiler.dir/App.cpp.o.d"
+  "/home/bernardo/projects/cpp_projects/MedusaCompiler/lexer.cpp" "CMakeFiles/MedusaCompiler.dir/lexer.cpp.o" "gcc" "CMakeFiles/MedusaCompiler.dir/lexer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/service/lexer/lexer.h
@@ -0,0 +1,23 @@
+#include <string>
+#ifndef LEXER_H
+#define LEXER_H
+
+enum Token{
+
+    //eof token
+    tok_eof = -1,
+
+    // commands
+    tok_def = -2,
+    tok_extern = -3,
+
+    //primary
+    tok_identifier = -4,
+    tok_number = -5,
+};
+
+extern std::string IdentifierStr; // Filled in if tok_identifier
+extern double NumVal;             // Filled in if tok_number
+
+int gettok();
+#endif
